45a83f9d590f94eb5c13fdbe2c13ad5f39654fe4
Grant Cohoe <grant@grantcohoe.com>
2020-01-19 18:29:13 -0500

    config

--- functions/library/cocktails.py
+++ functions/library/cocktails.py
@@ -1,6 +1,30 @@
 import json
+import barbados.config
 from barbados.models import CocktailModel
 from barbados.factories import CocktailFactory
+from barbados.connectors import RedisConnector
+
+
+def _list(event, context):
+    redis = RedisConnector()
+    try:
+        cocktail_name_list = redis.get(barbados.config.cache.cocktail_name_list_key)
+        response = {
+            'statusCode': 200,
+            'body': json.dumps(json.loads(cocktail_name_list))
+        }
+    except KeyError:
+        response = {
+            'statusCode': 502,
+            'body': 'Cache empty or other Redis error.'
+        }
+    except Exception as e:
+        response = {
+            'statusCode': 500,
+            'body': str(e)
+        }
+
+    return response
 
 
 def get(event, context):
